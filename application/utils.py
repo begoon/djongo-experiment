3c1df58225aa569bbcc92fab04e9d3b45d1c1968
Alexander Demin <ademin@gmail.com>
2022-07-08 23:56:49 +0100

    Fix mask_database_url_password

--- application/utils.py
+++ application/utils.py
@@ -14,12 +14,12 @@ logger = kwlog.logger(__name__)
 
 def mask_database_url_password(url: str) -> str:
     parts = url.split(":")
-    assert len(parts) == 4
+    assert len(parts) >= 3, parts
     parts = parts[2].split('@')
-    assert len(parts) == 2
+    assert len(parts) == 2, parts
     password = parts[0]
     updated_url = url.replace(password, "***")
-    assert url != updated_url
+    assert url != updated_url, f'{url=} == {updated_url=}'
     return updated_url
 
 
